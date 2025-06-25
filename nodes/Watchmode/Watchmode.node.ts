655d2f80f8e787927542f5bb0f45d6f63345b866
yambal <yambal@gmail.com>
2025-06-25 16:25:38 +0900

    Search

--- nodes/Watchmode/Watchmode.node.ts
+++ nodes/Watchmode/Watchmode.node.ts
@@ -45,29 +45,14 @@ export class Watchmode implements INodeType {
         noDataExpression: true,
         options: [
           {
-            name: 'Sources',
-            value: 'Sources',
-            action: 'Sources',
-            description: 'Provides a list of streaming sources with region info, logos, and app links, including subscription, rental, and free services',
+            name: 'Genres',
+            value: 'Genres',
+            action: 'Genres',
+            description: 'Return a mapping of genre names and IDs. Some genres have a tmdb_id, which is the corresponding genre ID on TheMovieDB.org API.',
             routing: {
               request: {
                 method: 'GET',
-                url: '/v1/sources/',
-                qs: {
-                  output: 'json',
-                }
-              }
-            }
-          },
-          {
-            name: 'Regions',
-            value: 'Regions',
-            action: 'Regions',
-            description: 'Return a listing of all regions (countries) that Watchmode currently supports. Each region includes information about data quality tier and plan availability.',
-            routing: {
-              request: {
-                method: 'GET',
-                url: '/v1/regions/',
+                url: '/v1/genres/',
                 qs: {
                   output: 'json',
                 }
@@ -90,14 +75,29 @@ export class Watchmode implements INodeType {
             }
           },
           {
-            name: 'Genres',
-            value: 'Genres',
-            action: 'Genres',
-            description: 'Return a mapping of genre names and IDs. Some genres have a tmdb_id, which is the corresponding genre ID on TheMovieDB.org API.',
+            name: 'Sources',
+            value: 'Sources',
+            action: 'Sources',
+            description: 'Provides a list of streaming sources with region info, logos, and app links, including subscription, rental, and free services',
             routing: {
               request: {
                 method: 'GET',
-                url: '/v1/genres/',
+                url: '/v1/sources/',
+                qs: {
+                  output: 'json',
+                }
+              }
+            }
+          },
+          {
+            name: 'Search',
+            value: 'Search',
+            action: 'Search',
+            description: 'Search for titles or people by name or external ID (e.g. IMDb, TMDB)',
+            routing: {
+              request: {
+                method: 'GET',
+                url: '/v1/search/',
                 qs: {
                   output: 'json',
                 }
@@ -107,160 +107,69 @@ export class Watchmode implements INodeType {
         ]
       },
       {
-				displayName: 'Geocode Query',
-				name: 'geocodeQuery',
+				displayName: 'Search Field',
+				name: 'searchField',
 				type: 'string',
-        default: '東京都港区六本木',
-        description: 'Address to get location information',
+        default: 'name',
+        description: 'Specifies the field to search: choose from imdb_id, tmdb_person_id, tmdb_movie_id, tmdb_tv_id, or name to find matching titles or people',
+        noDataExpression: false,
+        required: true,
+        routing: {
+          request: {
+            qs: {
+              search_field: '={{$parameter.searchField}}',
+            }
+          }
+        },
+        displayOptions: {
+          show: {
+            operation: [
+              'Search',
+            ]
+          }
+        }
+      },
+      {
+				displayName: 'Search Value',
+				name: 'searchValue',
+				type: 'string',
+        default: 'Ed Wood',
+        description: 'Specifies the value to search for, based on the chosen search_field. For example, if search_field is imdb_id, use an IMDb ID like tt0944947.',
+        noDataExpression: false,
+        required: true,
+        routing: {
+          request: {
+            qs: {
+              search_value: '={{$parameter.searchValue}}',
+            }
+          }
+        },
+        displayOptions: {
+          show: {
+            operation: [
+              'Search',
+            ]
+          }
+        }
+      },
+      {
+				displayName: 'Types',
+				name: 'types',
+				type: 'string',
+        default: '',
+        description: 'Specifies the type(s) to return. Use tv, movie, or person, or a comma-separated list (e.g. movie,tv) to filter results by type.',
         noDataExpression: false,
         routing: {
           request: {
             qs: {
-              query: '={{$parameter.geocodeQuery}}',
+              search_value: '={{$parameter.type}}',
             }
           }
         },
         displayOptions: {
           show: {
             operation: [
-              'Geocode',
-            ]
-          }
-        }
-			},
-      {
-        displayName: 'Address Level',
-        name: 'geocodeAddressLevel',
-        type: 'options',
-        default: '3',
-        description: 'Address detail level',
-        options: [
-          {
-            name: 'Prefecture Level',
-            value: '1',
-          },
-          {
-            name: 'Municipality Level',
-            value: '2',
-          },
-          {
-            name: 'Town or District Level',
-            value: '3',
-          },
-          {
-            name: 'Block or Subdistrict Level',
-            value: '4',
-          }
-        ],
-        routing: {
-          request: {
-            qs: {
-              al: '={{$parameter.geocodeAddressLevel}}',
-            }
-          }
-        },
-        displayOptions: {
-          show: {
-            operation: [
-              'Geocode',
-            ]
-          }
-        }
-      },
-      {
-        displayName: 'Address Range',
-        name: 'geocodeAddressRange',
-        type: 'options',
-        default: 'le',
-        description: 'Search scope of address levels',
-        options: [
-          {
-            name: 'Greater Equal',
-            value: 'ge',
-          },
-          {
-            name: 'Less Equal',
-            value: 'le',
-          },
-          {
-            name: 'Equal',
-            value: 'eq',
-          }
-        ],
-        routing: {
-          request: {
-            qs: {
-              ar: '={{$parameter.geocodeAddressRange}}',
-            }
-          }
-        },
-        displayOptions: {
-          show: {
-            operation: [
-              'Geocode',
-            ]
-          }
-        }
-      },
-      {
-        displayName: 'Results',
-        name: 'geocodeResults',
-        type: 'number',
-        default: 10,
-        description: 'Number of results to return',
-        routing: {
-          request: {
-            qs: {
-              results: '={{$parameter.geocodeResults}}',
-            }
-          }
-        },
-        displayOptions: {
-          show: {
-            operation: [
-              'Geocode',
-            ]
-          }
-        }
-      },
-      {
-        displayName: 'Latetude',
-        name: 'reverseGeoCoderLatetude',
-        type: 'number',
-        default: 35,
-        description: 'Is Latetude',
-        routing: {
-          request: {
-            qs: {
-              lat: '={{$parameter.reverseGeoCoderLatetude}}',
-            }
-          }
-        },
-        displayOptions: {
-          show: {
-            operation: [
-              'Reverse Geocoder',
-            ]
-          }
-        }
-      },
-      {
-        displayName: 'Longitude',
-        name: 'reverseGeoCoderLongitude',
-        type: 'number',
-        default: 135,
-        description: 'Is Longitude',
-        routing: {
-          request: {
-            qs: {
-              lon: '={{$parameter.reverseGeoCoderLongitude}}',
-            }
-          }
-        },
-        displayOptions: {
-          show: {
-            operation: [
-              'Reverse Geocoder',
+              'Search',
             ]
           }
         }
